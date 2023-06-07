eeeaed37e77afa19ecbc5de2789abbb1e14d923a
Titouan Hirsch <titouanhirsch@gmail.com>
2023-06-08 01:21:57 +0200

    search bar and cards done

--- db/migrate/20230607130021_change_name_in_roles.rb
+++
@@ -1,6 +0,0 @@
-class ChangeNameInRoles < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :roles, :name
-    add_column :roles, :recruiter, :boolean, default: false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_07_130021) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_06_135812) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,12 +66,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_07_130021) do
   end
 
   create_table "roles", force: :cascade do |t|
+    t.boolean "recruiter", default: false
     t.boolean "admin"
     t.bigint "company_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "recruiter", default: false
     t.index ["company_id"], name: "index_roles_on_company_id"
     t.index ["user_id"], name: "index_roles_on_user_id"
   end
@@ -86,7 +86,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_07_130021) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.boolean "available"
+    t.boolean "available", default: true
     t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
