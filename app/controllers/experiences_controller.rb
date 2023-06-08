38c773b0565523d6e8961aa9833c3aaa6287b6c2
Bazik1677 <125483625+Bazik1677@users.noreply.github.com>
2023-06-08 15:39:42 +0200

    controllerexp

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,12 +1,31 @@
 class ExperiencesController < ApplicationController
 
   def create
+    @user = User.find(params[:user_id])
+    @experience = Experience.new(experience_params)
+    @experience.user = @user
+    if @experience.save
+      redirect_to user_path(@user)
+    else
+      render "users/show", status: :unprocessable_entity
+    end
   end
 
   def update
+    @experience = Experience.find(params[:id])
+    if @experience.update(experience_params)
+      redirect_to user_path, notice: "experience updated"
+    else
+      render :dashboard, status: :unprocessable_entity
+    end
   end
 
   def destroy
   end
-  
+
+  def experience_params
+    #ajouter la photo du logo de l'entreprise dans les permits quand ce sera done sur la seed.
+    params.require(:experience).permit(:job_name, :job_description, :start_date, :end_date, :skills, :company_id)
+  end
+
 end
