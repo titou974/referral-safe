c8d6dc459d84af074315667e6911a40f187c3793
loic hamerschmidt <loich95@gmail.com>
2023-06-09 16:13:36 +0200

    finish role form

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,6 +1,10 @@
 class RolesController < ApplicationController
 
   def create
+    @role = Role.new(role_params)
+    @role.user = current_user
+    @role.save
+    redirect_to user_path(User.find(params[:user_id]))
   end
 
   def update
@@ -8,5 +12,11 @@ class RolesController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+
+  def role_params
+    params.require(:role).permit(:recruiter, :company_id)
   end
 end
