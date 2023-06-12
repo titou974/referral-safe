5a7b3819201183b2011a0d1461367fa75d926447
Bazik1677 <125483625+Bazik1677@users.noreply.github.com>
2023-06-12 11:49:18 +0200

    chat

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,11 @@ Rails.application.routes.draw do
   end
   resources :companies, only: [:update, :destroy]
 
+  resources :chatrooms, only: :show do
+    resources :messages, only: :create
+  end
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
