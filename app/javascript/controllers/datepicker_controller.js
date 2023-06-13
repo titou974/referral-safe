3268c0f4660e2846d248d658ab92491fa4a6b01b
Antoine Vassort <antoine.clean@gmail.com>
2023-06-13 13:11:01 +0200

    AjotuFooterSlider/DatePickr/ModifHome&Navbar

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -7,7 +7,10 @@ import flatpickr from "flatpickr";
 export default class extends Controller {
   connect() {
     flatpickr(this.element, {
-      mode: "range"
+      mode: "range",
+      altInput: true,
+      altFormat: "F j, Y",
+      dateFormat: "Y-m-d"
     })
   }
 }
