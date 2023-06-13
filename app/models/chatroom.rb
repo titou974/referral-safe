5a8b3f27005d1ab8c3ac1c483b73bf1a0100ada1
Bazik1677 <125483625+Bazik1677@users.noreply.github.com>
2023-06-13 16:54:28 +0200

    chatbar

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,3 +1,5 @@
 class Chatroom < ApplicationRecord
   has_many :messages
+  belongs_to :user_a, class_name: 'User', foreign_key: :user_a_id
+  belongs_to :user_b, class_name: 'User', foreign_key: :user_b_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_one :role, dependent: :destroy
   has_many :experiences, dependent: :destroy
   has_many :companies, through: :experiences
+  has_many :chatrooms, ->(user) { unscope(where: :user_id).where("chatrooms.user_a_id = :id OR chatrooms.user_b_id = :id", id: user.id) }, class_name: 'Chatroom', dependent: :destroy
   has_one_attached :photo
   has_one :company, through: :role
 
