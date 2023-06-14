466ce9792502ea33559825fe4348e912deb63cbd
Titouan Hirsch <titouanhirsch@gmail.com>
2023-06-14 16:50:51 +0200

    search tom js

--- app/javascript/controllers/autocomplete_controller.js
+++ app/javascript/controllers/autocomplete_controller.js
@@ -5,25 +5,16 @@ export default class extends Controller {
   connect() {
     console.log("hello")
     new TomSelect(this.element,{
+      plugins: {
+        remove_button:{
+          title:'Remove this item',
+        }
+      },
       persist: false,
       createOnBlur: true,
-      create: true,
-
-
-  })
-  new TomSelect('#input-tags',{
-    plugins: {
-      remove_button:{
-        title:'Remove this item',
-      }
-    },
-    persist: false,
-    create: true,
-    onDelete: function(values) {
-      return confirm(values.length > 1 ? 'Are you sure you want to remove these ' + values.length + ' items?' : 'Are you sure you want to remove "' + values[0] + '"?');
-    }
-  });
-}
+      create: true
+    })
+  }
 
 
 }
